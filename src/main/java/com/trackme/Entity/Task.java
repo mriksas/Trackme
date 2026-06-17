7c86b2669e3cb11998441dfb0bda02dcaf7737cb
Vadym <68250628+xgvfhf@users.noreply.github.com>
2026-06-17 19:40:50 +0300

    add setters/getters into task entity

--- src/main/java/com/trackme/Entity/Task.java
+++ src/main/java/com/trackme/Entity/Task.java
@@ -2,37 +2,41 @@ package com.trackme.Entity;
 import com.trackme.Enums.Priority;
 import com.trackme.Enums.Status;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long taskId;
+
+    @Setter
     private String taskName;
+
+    @Setter
     private String taskDescription;
+
+    @Setter
     private LocalDate startDate;
+
+    @Setter
     private LocalDate endDate;
 
     @Enumerated(EnumType.STRING)
+    @Setter
     private Priority taskPriority;
 
     @Enumerated(EnumType.STRING)
+    @Setter
     private Status taskStatus;
-
-    Task(Long taskId, String taskName, String taskDescription, LocalDate startDate, LocalDate endDate, Priority taskPriority, Status taskStatus)
-    {
-        this.taskId = taskId;
-        this.taskName = taskName;
-        this.taskDescription = taskDescription;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.taskPriority = taskPriority;
-        this.taskStatus = taskStatus;
-    }
-
-
-
+    
 }
