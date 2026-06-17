ec6c7d2670379cbde92a5be48b729cb95c16c9d7
Vadym <68250628+xgvfhf@users.noreply.github.com>
2026-06-17 22:03:09 +0300

    Add comments for clarity

--- src/main/java/com/trackme/Service/TaskService.java
+++ src/main/java/com/trackme/Service/TaskService.java
@@ -5,16 +5,17 @@ import com.trackme.Repository.TaskRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @AllArgsConstructor
 @Service
 public class TaskService {
     private TaskRepository taskRepository;
 
+    // Adding new task to db
     public Task saveTask(Task task)
     {
         return taskRepository.save(task);
     }
-
-
 
 }
