412766da678d6bfa5511e2320b98a2f29398bf96
Vadym <68250628+xgvfhf@users.noreply.github.com>
2026-06-17 22:04:19 +0300

    Add task addition initial functionality in TaskController

--- src/main/java/com/trackme/Controller/TaskController.java
+++ src/main/java/com/trackme/Controller/TaskController.java
@@ -4,13 +4,18 @@ import com.trackme.Entity.Task;
 import com.trackme.Service.TaskService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RequestMapping("/api/v1/tasks")
 @AllArgsConstructor
+@RestController
 public class TaskController {
     private TaskService taskService;
 
+    @PostMapping
     public Task addTask(@RequestBody Task task)
     {
         return taskService.saveTask(task);
