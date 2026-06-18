5b2a029cd52f98428239feab1d56ff56a9ecdadd
Vadym <68250628+xgvfhf@users.noreply.github.com>
2026-06-18 17:00:36 +0300

    Change TaskService logic putting dto's instead of entities

--- src/main/java/com/trackme/Service/TaskService.java
+++ src/main/java/com/trackme/Service/TaskService.java
@@ -1,6 +1,8 @@
 package com.trackme.Service;
 
+import com.trackme.Dto.CreateTaskRequest;
 import com.trackme.Entity.Task;
+import com.trackme.Interface.TaskMapper;
 import com.trackme.Repository.TaskRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,11 +13,17 @@ import java.util.List;
 @Service
 public class TaskService {
     private TaskRepository taskRepository;
-
+    private TaskMapper taskMapper;
     // Adding new task to db
-    public Task saveTask(Task task)
+    public Task saveTask(CreateTaskRequest createTaskRequest)
     {
+        var task = taskMapper.toTaskEntity(createTaskRequest);
         return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks()
+    {
+        return taskRepository.findAll();
     }
 
 }
